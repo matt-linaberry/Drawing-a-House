f9cc44c4cf08278e06942e9a028f5db821409060
Matt Linaberry <matt.linaberry@we-ics.com>
2014-12-11 20:45:56 -0500

    Initial Commit

---
+++ Drawing a House/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Drawing a House
+//
+//  Created by Matthew Linaberry on 12/11/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
